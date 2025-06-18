6c3c20e5b26e04fa9d72303798f4eeb5dfd8a0bc
poyanoredini <poya.ali.m@gmail.com>
2025-06-18 13:08:58 +0000

    create database config

---
+++ config/database.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: process.env.DB_PORT || 3306,
+        logging: false // Set to console.log to see SQL queries
+    }
+);
+
+// Test the connection
+sequelize.authenticate()
+    .then(() => {
+        console.log('Database connection established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+module.exports = sequelize;
